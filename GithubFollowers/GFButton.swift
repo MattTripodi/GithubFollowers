263956e68f94227b2b93b87302ddaaabf4718899
MattTripodi <mtripodi1027@gmail.com>
2024-03-07 20:31:40 -0500

    Create GFButton and GFTextField

---
+++ GithubFollowers/GFButton.swift
@@ -0,0 +1,41 @@
+//
+//  GFButton.swift
+//  GithubFollowers
+//
+//  Created by Matthew Tripodi on 3/7/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    // This is what handles initialization via Storyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Initializer to pass in whatever background color and title needed
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        // Conforming to dynamic type so that the text adjusts as needed as per the device settings
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        // This is basically to use Auto Layout
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
