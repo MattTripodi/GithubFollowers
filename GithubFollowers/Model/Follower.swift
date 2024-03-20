42d0052f0bde933e6f462cb48f55ed718f8f9752
MattTripodi <mtripodi1027@gmail.com>
2024-03-20 11:46:32 -0400

    Create Follower model and User Model

---
+++ GithubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GithubFollowers
+//
+//  Created by Matthew Tripodi on 3/20/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String 
+}
---
+++ GithubFollowers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GithubFollowers
+//
+//  Created by Matthew Tripodi on 3/20/24.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
