fa462fcef634cba2541fb1874d8f541e75d31518
MattTripodi <mtripodi1027@gmail.com>
2024-04-05 15:30:05 -0400

    Configure FollowerCell (CollectionView Cell) and configure GFAvatarImageView

---
+++ GithubFollowers/Views/GFAvatarImageView.swift
@@ -0,0 +1,31 @@
+//
+//  GFAvatarImageView.swift
+//  GithubFollowers
+//
+//  Created by Matthew Tripodi on 4/2/24.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeHolderImage = UIImage(named: "avatar-placeholder")!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeHolderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
