05727d66889186f364474b8c49813915ec4e6e7b
MattTripodi <mtripodi1027@gmail.com>
2024-08-21 15:14:31 -0400

    Create GFRepoItemVC and GFFollowerItemVC - Update GFButton with set function - Update GFItemInfoVC with user init - Update UserInfoVC to populate GFRepoItemVC and GFFollowerItemVC with user data

---
+++ GithubFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,24 @@
+//
+//  GFFollowerItemVC.swift
+//  GithubFollowers
+//
+//  Created by Matthew Tripodi on 8/21/24.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+    
+}
